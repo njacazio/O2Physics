5ccfa89d58dee468a756929277c489dc3b429762
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2022-02-08 19:12:48 +0100

    Update spectra tasks

--- PWGLF/Tasks/spectraTOF.cxx
+++ PWGLF/Tasks/spectraTOF.cxx
@@ -10,8 +10,8 @@
 // or submit itself to any jurisdiction.
 
 ///
-/// \file   spectraTOF.h
-/// \author Nicolo' Jacazio
+/// \file   spectraTOF.cxx
+/// \author Nicolò Jacazio nicolo.jacazio@cern.ch
 ///
 /// \brief Task for the analysis of the spectra with the TOF detector
 ///
@@ -23,8 +23,11 @@
 #include "Framework/HistogramRegistry.h"
 #include "Common/Core/PID/PIDResponse.h"
 #include "Common/DataModel/TrackSelectionTables.h"
+#include "Common/Core/TrackSelection.h"
+#include "Common/Core/TrackSelectionDefaults.h"
 
 using namespace o2;
+using namespace o2::track;
 using namespace o2::framework;
 using namespace o2::framework::expressions;
 
@@ -32,13 +35,44 @@ using namespace o2::framework::expressions;
 struct tofSpectra {
   static constexpr int Np = 9;
   static constexpr const char* pT[Np] = {"e", "#mu", "#pi", "K", "p", "d", "t", "^{3}He", "#alpha"};
-  static constexpr std::string_view hp[Np] = {"p/El", "p/Mu", "p/Pi", "p/Ka", "p/Pr", "p/De", "p/Tr", "p/He", "p/Al"};
-  static constexpr std::string_view hpt[Np] = {"pt/El", "pt/Mu", "pt/Pi", "pt/Ka", "pt/Pr", "pt/De", "pt/Tr", "pt/He", "pt/Al"};
+  Configurable<float> cfgNSigmaCut{"cfgNSigmaCut", 3, "Value of the Nsigma cut"};
+  Configurable<float> cfgCutVertex{"cfgCutVertex", 10.0f, "Accepted z-vertex range"};
+  Configurable<float> cfgCutEta{"cfgCutEta", 0.8f, "Eta range for tracks"};
+  Configurable<int> nBinsP{"nBinsP", 3000, "Number of bins for the momentum"};
+  Configurable<float> minP{"minP", 0.01, "Minimum momentum in range"};
+  Configurable<float> maxP{"maxP", 20, "Maximum momentum in range"};
+  Configurable<bool> isRun2{"isRun2", false, "Flag to process Run 2 data"};
+
+  // Histograms
   HistogramRegistry histos{"Histos", {}, OutputObjHandlingPolicy::AnalysisObject};
+  static constexpr std::string_view hp[Np] = {"p/El", "p/Mu", "p/Pi",
+                                              "p/Ka", "p/Pr", "p/De",
+                                              "p/Tr", "p/He", "p/Al"};
+  static constexpr std::string_view hpt[Np] = {"pt/El", "pt/Mu", "pt/Pi",
+                                               "pt/Ka", "pt/Pr", "pt/De",
+                                               "pt/Tr", "pt/He", "pt/Al"};
+  static constexpr std::string_view hdcaxy[Np] = {"dcaxy/El", "dcaxy/Mu", "dcaxy/Pi",
+                                                  "dcaxy/Ka", "dcaxy/Pr", "dcaxy/De",
+                                                  "dcaxy/Tr", "dcaxy/He", "dcaxy/Al"};
+  static constexpr std::string_view hdcaz[Np] = {"dcaz/El", "dcaz/Mu", "dcaz/Pi",
+                                                 "dcaz/Ka", "dcaz/Pr", "dcaz/De",
+                                                 "dcaz/Tr", "dcaz/He", "dcaz/Al"};
+
+  TrackSelection globalTrackswoPrim; // Track without cut for primaries
 
   void init(o2::framework::InitContext&)
   {
+    globalTrackswoPrim = getGlobalTrackSelection();
+    globalTrackswoPrim.SetMaxDcaXYPtDep([](float pt) { return 3.f + pt; });
+    globalTrackswoPrim.SetRequireGoldenChi2(false);
+    if (!isRun2) {
+      globalTrackswoPrim.SetTrackType(o2::aod::track::TrackTypeEnum::Track);
+    }
+
     const AxisSpec vtxZAxis{100, -20, 20, "Vtx_{z} (cm)"};
+    const AxisSpec pAxis{nBinsP, minP, maxP, "#it{p} (GeV/#it{c})"};
+    const AxisSpec ptAxis{nBinsP, minP, maxP, "#it{p}_{T} (GeV/#it{c})"};
+
     histos.add("event/vertexz", "", HistType::kTH1F, {vtxZAxis});
     auto h = histos.add<TH1>("evsel", "evsel", HistType::kTH1F, {{10, 0.5, 10.5}});
     h->GetXaxis()->SetBinLabel(1, "Events read");
@@ -48,40 +82,57 @@ struct tofSpectra {
     h->GetXaxis()->SetBinLabel(2, "Eta passed");
     h->GetXaxis()->SetBinLabel(3, "Quality passed");
     h->GetXaxis()->SetBinLabel(4, "TOF passed");
-    h->GetXaxis()->SetBinLabel(5, "TrackTimeRes passed");
-    histos.add("p/Unselected", "Unselected;#it{p} (GeV/#it{c})", kTH1F, {{100, 0, 20}});
-    histos.add("pt/Unselected", "Unselected;#it{p}_{T} (GeV/#it{c})", kTH1F, {{100, 0, 20}});
+    histos.add("p/Unselected", "Unselected", kTH1F, {pAxis});
+    histos.add("pt/Unselected", "Unselected", kTH1F, {ptAxis});
     for (int i = 0; i < Np; i++) {
-      histos.add(hp[i].data(), Form("%s;#it{p} (GeV/#it{c})", pT[i]), kTH1F, {{100, 0, 20}});
-      histos.add(hpt[i].data(), Form("%s;#it{p}_{T} (GeV/#it{c})", pT[i]), kTH1F, {{100, 0, 20}});
+      histos.add(hp[i].data(), pT[i], kTH1F, {pAxis});
+      histos.add(hpt[i].data(), pT[i], kTH1F, {ptAxis});
     }
-    histos.add("electronbeta/hp_El", ";#it{p} (GeV/#it{c})", kTH1F, {{100, 0, 20}});
-    histos.add("electronbeta/hpt_El", ";#it{p}_{T} (GeV/#it{c})", kTH1F, {{100, 0, 20}});
+    histos.add("electronbeta/hp_El", "", kTH1F, {pAxis});
+    histos.add("electronbeta/hpt_El", "", kTH1F, {ptAxis});
     histos.add("electronbeta/hlength_El", ";Track Length (cm);Tracks", kTH1D, {{100, 0, 1000}});
     histos.add("electronbeta/htime_El", ";TOF Time (ns);Tracks", kTH1D, {{1000, 0, 600}});
-    histos.add("electronbeta/hp_beta_El", ";#it{p} (GeV/#it{c});#beta - #beta_{e};Tracks", kTH2D, {{100, 0, 20}, {100, -0.01, 0.01}});
-    histos.add("electronbeta/hp_betasigma_El", ";#it{p} (GeV/#it{c});(#beta - #beta_{e})/#sigma;Tracks", kTH2D, {{100, 0, 20}, {100, -5, 5}});
+    histos.add("electronbeta/hp_beta_El", ";#it{p} (GeV/#it{c});#beta - #beta_{e};Tracks", kTH2D, {pAxis, {100, -0.01, 0.01}});
+    histos.add("electronbeta/hp_betasigma_El", ";#it{p} (GeV/#it{c});(#beta - #beta_{e})/#sigma;Tracks", kTH2D, {pAxis, {100, -5, 5}});
+
+    // DCAxy
+    const AxisSpec dcaXyAxis{600, -3.005, 2.995, "DCA_{xy} (cm)"};
+    const AxisSpec dcaZAxis{600, -3.005, 2.995, "DCA_{z} (cm)"};
+    for (int i = 0; i < Np; i++) {
+      histos.add(hdcaxy[i].data(), pT[i], kTH2F, {ptAxis, dcaXyAxis});
+      histos.add(hdcaz[i].data(), pT[i], kTH2F, {ptAxis, dcaZAxis});
+    }
   }
 
-  template <std::size_t i, typename T>
-  void fillParticleHistos(const T& track, const float& nsigma)
+  template <PID::ID id, typename T>
+  void fillParticleHistos(const T& track)
   {
+    const float y = TMath::ASinH(track.pt() / TMath::Sqrt(PID::getMass2(id) + track.pt() * track.pt()) * TMath::SinH(track.eta()));
+    if (abs(y) > 0.5) {
+      return;
+    }
+    const auto& nsigma = o2::aod::pidutils::tofNSigma<id>(track);
+    if (std::abs(nsigma) < 2) {
+      histos.fill(HIST(hdcaxy[id]), track.pt(), track.dcaXY());
+      histos.fill(HIST(hdcaz[id]), track.pt(), track.dcaZ());
+    }
+    if (!track.isGlobalTrack()) {
+      return;
+    }
     if (abs(nsigma) > cfgNSigmaCut) {
       return;
     }
-    histos.fill(HIST(hp[i]), track.p());
-    histos.fill(HIST(hpt[i]), track.pt());
+    histos.fill(HIST(hp[id]), track.p());
+    histos.fill(HIST(hpt[id]), track.pt());
   }
 
-  //Defining filters and input
-  Configurable<float> cfgNSigmaCut{"cfgNSigmaCut", 3, "Value of the Nsigma cut"};
-  Configurable<float> cfgCutVertex{"cfgCutVertex", 10.0f, "Accepted z-vertex range"};
-  Configurable<float> cfgCutEta{"cfgCutEta", 0.8f, "Eta range for tracks"};
-  using TrackCandidates = soa::Join<aod::Tracks, aod::TracksExtra,
+  using TrackCandidates = soa::Join<aod::Tracks, aod::TracksExtra, aod::TracksExtended,
                                     aod::pidTOFFullEl, aod::pidTOFFullMu, aod::pidTOFFullPi,
                                     aod::pidTOFFullKa, aod::pidTOFFullPr, aod::pidTOFFullDe,
                                     aod::pidTOFFullTr, aod::pidTOFFullHe, aod::pidTOFFullAl,
-                                    aod::pidTOFbeta, aod::TOFSignal, aod::TrackSelection>;
+                                    aod::pidTOFbeta, aod::TOFSignal,
+                                    aod::TrackSelection>;
+
   void process(aod::Collision const& collision,
                TrackCandidates const& tracks)
   {
@@ -98,7 +149,7 @@ struct tofSpectra {
         continue;
       }
       histos.fill(HIST("tracksel"), 2);
-      if (!track.isGlobalTrack()) {
+      if (!globalTrackswoPrim.IsSelected(track)) {
         continue;
       }
       histos.fill(HIST("tracksel"), 3);
@@ -106,25 +157,19 @@ struct tofSpectra {
         continue;
       }
       histos.fill(HIST("tracksel"), 4);
-      if (track.trackType() == o2::aod::track::TrackTypeEnum::Run2Track) { // check fTrackTimeRes, this can removed for conversions of Run2 data done with Aliphysics later than vAN-20210917
-        if (track.trackTimeRes() <= 0.f || track.trackTimeRes() > 250e-3f) {
-          continue;
-        }
-      }
-      histos.fill(HIST("tracksel"), 5);
 
       histos.fill(HIST("p/Unselected"), track.p());
       histos.fill(HIST("pt/Unselected"), track.pt());
 
-      fillParticleHistos<0>(track, track.tofNSigmaEl());
-      fillParticleHistos<1>(track, track.tofNSigmaMu());
-      fillParticleHistos<2>(track, track.tofNSigmaPi());
-      fillParticleHistos<3>(track, track.tofNSigmaKa());
-      fillParticleHistos<4>(track, track.tofNSigmaPr());
-      fillParticleHistos<5>(track, track.tofNSigmaDe());
-      fillParticleHistos<6>(track, track.tofNSigmaTr());
-      fillParticleHistos<7>(track, track.tofNSigmaHe());
-      fillParticleHistos<8>(track, track.tofNSigmaAl());
+      fillParticleHistos<PID::Electron>(track);
+      fillParticleHistos<PID::Muon>(track);
+      fillParticleHistos<PID::Pion>(track);
+      fillParticleHistos<PID::Kaon>(track);
+      fillParticleHistos<PID::Proton>(track);
+      fillParticleHistos<PID::Deuteron>(track);
+      fillParticleHistos<PID::Triton>(track);
+      fillParticleHistos<PID::Helium3>(track);
+      fillParticleHistos<PID::Alpha>(track);
 
       //
       if (TMath::Abs(track.separationbetael() < 1.f)) {
@@ -137,7 +182,7 @@ struct tofSpectra {
       }
     }
   } // end of the process function
-};
+};  // end of spectra task
 
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
 {
--- PWGLF/Tasks/spectraTPC.cxx
+++ PWGLF/Tasks/spectraTPC.cxx
@@ -10,8 +10,8 @@
 // or submit itself to any jurisdiction.
 
 ///
-/// \file   spectraTPC.h
-/// \author Nicolo' Jacazio
+/// \file   spectraTPC.cxx
+/// \author Nicolò Jacazio nicolo.jacazio@cern.ch
 ///
 /// \brief Task for the analysis of the spectra with the TPC detector
 ///        In addition the task makes histograms of the TPC signal with TOF selections.
@@ -23,8 +23,11 @@
 #include "Framework/HistogramRegistry.h"
 #include "Common/Core/PID/PIDResponse.h"
 #include "Common/DataModel/TrackSelectionTables.h"
+#include "Common/Core/TrackSelection.h"
+#include "Common/Core/TrackSelectionDefaults.h"
 
 using namespace o2;
+using namespace o2::track;
 using namespace o2::framework;
 using namespace o2::framework::expressions;
 
@@ -41,13 +44,44 @@ void customize(std::vector<o2::framework::ConfigParamSpec>& workflowOptions)
 struct tpcSpectra {
   static constexpr int Np = 9;
   static constexpr const char* pT[Np] = {"e", "#mu", "#pi", "K", "p", "d", "t", "^{3}He", "#alpha"};
-  static constexpr std::string_view hp[Np] = {"p/El", "p/Mu", "p/Pi", "p/Ka", "p/Pr", "p/De", "p/Tr", "p/He", "p/Al"};
-  static constexpr std::string_view hpt[Np] = {"pt/El", "pt/Mu", "pt/Pi", "pt/Ka", "pt/Pr", "pt/De", "pt/Tr", "pt/He", "pt/Al"};
+  Configurable<float> cfgNSigmaCut{"cfgNSigmaCut", 3, "Value of the Nsigma cut"};
+  Configurable<float> cfgCutVertex{"cfgCutVertex", 10.0f, "Accepted z-vertex range"};
+  Configurable<float> cfgCutEta{"cfgCutEta", 0.8f, "Eta range for tracks"};
+  Configurable<int> nBinsP{"nBinsP", 3000, "Number of bins for the momentum"};
+  Configurable<float> minP{"minP", 0.01, "Minimum momentum in range"};
+  Configurable<float> maxP{"maxP", 20, "Maximum momentum in range"};
+  Configurable<bool> isRun2{"isRun2", false, "Flag to process Run 2 data"};
+
+  // Histograms
   HistogramRegistry histos{"Histos", {}, OutputObjHandlingPolicy::AnalysisObject};
+  static constexpr std::string_view hp[Np] = {"p/El", "p/Mu", "p/Pi",
+                                              "p/Ka", "p/Pr", "p/De",
+                                              "p/Tr", "p/He", "p/Al"};
+  static constexpr std::string_view hpt[Np] = {"pt/El", "pt/Mu", "pt/Pi",
+                                               "pt/Ka", "pt/Pr", "pt/De",
+                                               "pt/Tr", "pt/He", "pt/Al"};
+  static constexpr std::string_view hdcaxy[Np] = {"dcaxy/El", "dcaxy/Mu", "dcaxy/Pi",
+                                                  "dcaxy/Ka", "dcaxy/Pr", "dcaxy/De",
+                                                  "dcaxy/Tr", "dcaxy/He", "dcaxy/Al"};
+  static constexpr std::string_view hdcaz[Np] = {"dcaz/El", "dcaz/Mu", "dcaz/Pi",
+                                                 "dcaz/Ka", "dcaz/Pr", "dcaz/De",
+                                                 "dcaz/Tr", "dcaz/He", "dcaz/Al"};
+
+  TrackSelection globalTrackswoPrim; // Track without cut for primaries
 
   void init(o2::framework::InitContext&)
   {
+    globalTrackswoPrim = getGlobalTrackSelection();
+    globalTrackswoPrim.SetMaxDcaXYPtDep([](float pt) { return 3.f + pt; });
+    globalTrackswoPrim.SetRequireGoldenChi2(false);
+    if (!isRun2) {
+      globalTrackswoPrim.SetTrackType(o2::aod::track::TrackTypeEnum::Track);
+    }
+
     const AxisSpec vtxZAxis{100, -20, 20, "Vtx_{z} (cm)"};
+    const AxisSpec pAxis{nBinsP, minP, maxP, "#it{p} (GeV/#it{c})"};
+    const AxisSpec ptAxis{nBinsP, minP, maxP, "#it{p}_{T} (GeV/#it{c})"};
+
     histos.add("event/vertexz", "", HistType::kTH1F, {vtxZAxis});
     auto h = histos.add<TH1>("evsel", "evsel", HistType::kTH1F, {{10, 0.5, 10.5}});
     h->GetXaxis()->SetBinLabel(1, "Events read");
@@ -56,33 +90,50 @@ struct tpcSpectra {
     h->GetXaxis()->SetBinLabel(1, "Tracks read");
     h->GetXaxis()->SetBinLabel(2, "Eta passed");
     h->GetXaxis()->SetBinLabel(3, "Quality passed");
-    histos.add("p/Unselected", "Unselected;#it{p} (GeV/#it{c})", kTH1F, {{100, 0, 20}});
-    histos.add("pt/Unselected", "Unselected;#it{p}_{T} (GeV/#it{c})", kTH1F, {{100, 0, 20}});
+    histos.add("p/Unselected", "Unselected", kTH1F, {pAxis});
+    histos.add("pt/Unselected", "Unselected", kTH1F, {ptAxis});
     for (int i = 0; i < Np; i++) {
-      histos.add(hp[i].data(), Form("%s;#it{p} (GeV/#it{c})", pT[i]), kTH1F, {{100, 0, 20}});
-      histos.add(hpt[i].data(), Form("%s;#it{p}_{T} (GeV/#it{c})", pT[i]), kTH1F, {{100, 0, 20}});
+      histos.add(hp[i].data(), pT[i], kTH1F, {pAxis});
+      histos.add(hpt[i].data(), pT[i], kTH1F, {ptAxis});
+    }
+
+    // DCAxy
+    const AxisSpec dcaXyAxis{600, -3.005, 2.995, "DCA_{xy} (cm)"};
+    const AxisSpec dcaZAxis{600, -3.005, 2.995, "DCA_{z} (cm)"};
+    for (int i = 0; i < Np; i++) {
+      histos.add(hdcaxy[i].data(), pT[i], kTH2F, {ptAxis, dcaXyAxis});
+      histos.add(hdcaz[i].data(), pT[i], kTH2F, {ptAxis, dcaZAxis});
     }
   }
 
-  template <std::size_t i, typename T>
-  void fillParticleHistos(const T& track, const float& nsigma)
+  template <PID::ID id, typename T>
+  void fillParticleHistos(const T& track)
   {
+    const float y = TMath::ASinH(track.pt() / TMath::Sqrt(PID::getMass2(id) + track.pt() * track.pt()) * TMath::SinH(track.eta()));
+    if (abs(y) > 0.5) {
+      return;
+    }
+    const auto& nsigma = o2::aod::pidutils::tpcNSigma<id>(track);
+    if (std::abs(nsigma) < 2) {
+      histos.fill(HIST(hdcaxy[id]), track.pt(), track.dcaXY());
+      histos.fill(HIST(hdcaz[id]), track.pt(), track.dcaZ());
+    }
+    if (!track.isGlobalTrack()) {
+      return;
+    }
     if (abs(nsigma) > cfgNSigmaCut) {
       return;
     }
-    histos.fill(HIST(hp[i]), track.p());
-    histos.fill(HIST(hpt[i]), track.pt());
+    histos.fill(HIST(hp[id]), track.p());
+    histos.fill(HIST(hpt[id]), track.pt());
   }
 
-  //Defining filters and input
-  Configurable<float> cfgNSigmaCut{"cfgNSigmaCut", 3, "Value of the Nsigma cut"};
-  Configurable<float> cfgCutVertex{"cfgCutVertex", 10.0f, "Accepted z-vertex range"};
-  Configurable<float> cfgCutEta{"cfgCutEta", 0.8f, "Eta range for tracks"};
-  using TrackCandidates = soa::Join<aod::Tracks, aod::TracksExtra,
+  using TrackCandidates = soa::Join<aod::Tracks, aod::TracksExtra, aod::TracksExtended,
                                     aod::pidTPCFullEl, aod::pidTPCFullMu, aod::pidTPCFullPi,
                                     aod::pidTPCFullKa, aod::pidTPCFullPr, aod::pidTPCFullDe,
                                     aod::pidTPCFullTr, aod::pidTPCFullHe, aod::pidTPCFullAl,
                                     aod::TrackSelection>;
+
   void process(aod::Collision const& collision,
                TrackCandidates const& tracks)
   {
@@ -99,7 +150,7 @@ struct tpcSpectra {
         continue;
       }
       histos.fill(HIST("tracksel"), 2);
-      if (!track.isGlobalTrack()) {
+      if (!globalTrackswoPrim.IsSelected(track)) {
         continue;
       }
       histos.fill(HIST("tracksel"), 3);
@@ -107,18 +158,18 @@ struct tpcSpectra {
       histos.fill(HIST("p/Unselected"), track.p());
       histos.fill(HIST("pt/Unselected"), track.pt());
 
-      fillParticleHistos<0>(track, track.tpcNSigmaEl());
-      fillParticleHistos<1>(track, track.tpcNSigmaMu());
-      fillParticleHistos<2>(track, track.tpcNSigmaPi());
-      fillParticleHistos<3>(track, track.tpcNSigmaKa());
-      fillParticleHistos<4>(track, track.tpcNSigmaPr());
-      fillParticleHistos<5>(track, track.tpcNSigmaDe());
-      fillParticleHistos<6>(track, track.tpcNSigmaTr());
-      fillParticleHistos<7>(track, track.tpcNSigmaHe());
-      fillParticleHistos<8>(track, track.tpcNSigmaAl());
+      fillParticleHistos<PID::Electron>(track);
+      fillParticleHistos<PID::Muon>(track);
+      fillParticleHistos<PID::Pion>(track);
+      fillParticleHistos<PID::Kaon>(track);
+      fillParticleHistos<PID::Proton>(track);
+      fillParticleHistos<PID::Deuteron>(track);
+      fillParticleHistos<PID::Triton>(track);
+      fillParticleHistos<PID::Helium3>(track);
+      fillParticleHistos<PID::Alpha>(track);
     }
   } // end of the process function
-};
+};  // end of spectra task
 
 struct tpcPidQaSignalwTof {
   static constexpr int Np = 9;
